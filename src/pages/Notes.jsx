d8c3aa820bb5d2d480e1155b7f9db815bedaf2f8
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-06-29 17:32:34 +0000

    notes-grid

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import { useState } from "react";
+import { BsSearchHeart } from "react-icons/bs";
+import dummyNotes from "../dummyNotes";
+import { Link } from "react-router-dom";
+import { BsPlusLg } from "react-icons/bs";
+import NoteItem from "../components/NoteItem";
+import randomColor from "randomcolor";
 
 const Notes = () => {
+  const [notes, setNotes] = useState([]);
+
+  const generateRandomColor = () => {
+    return randomColor();
+  };
+
+  const createNewNote = () => {
+    const newNote = {
+      id: notes.length + 1,
+      color: generateRandomColor(),
+    };
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
+
   return (
     <section>
-      <header className='notes__header'></header>
+      <header className="notes__header">
+        <h2>My Notes</h2>
+        {/* <input type="text" autoFocus placeholder='keyword...' /> */}
+        <button className="btn">
+          <BsSearchHeart />
+        </button>
+      </header>
+      <div className="notes__container">
+        {dummyNotes.map((note) => (
+          <NoteItem
+            key={note.id}
+            note={note}
+            style={{ backgroundColor: generateRandomColor() }}
+          />
+        ))}
+      </div>
+      <Link className="btn add__btn">
+        <BsPlusLg />
+      </Link>
     </section>
-  )
-}
+  );
+};
 
-export default Notes
+export default Notes;
